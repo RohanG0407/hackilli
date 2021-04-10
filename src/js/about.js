fe0ae9af1bdb3caad8f9ff70c9f0cc2336250255
James <james.d.menezes@gmail.com>
2021-04-10 03:49:35 -0500

    yo

--- src/js/about.js
+++ src/js/about.js
@@ -1,10 +1,31 @@
 import "../styling/about.scss"
-//import "../gsap/menu"
 
+const player = document.getElementById('player');
 
+var rec_pressed = false;
+const rec_but = document.getElementById("record");
 
+const handleSuccess = function(stream) {
+    const options = {mimeType: 'audio/webm'};
+    const mediaRecorder = new MediaRecorder(stream, options);
 
+    rec_but.addEventListener('mousedown', function(e) {
+        rec_pressed = true;
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+    });
 
+    rec_but.addEventListener('mouseup', function(e) {
+        rec_pressed = false;
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+    });
 
+    mediaRecorder.addEventListener('dataavailable', function(e) {
+        // send e.data
+        console.log(e.data)
+    });
+};
 
-
+navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(handleSuccess);
